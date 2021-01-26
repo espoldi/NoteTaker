93f49210d8bc9d280b635602def9b50a5240c17c
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-01-25 22:23:19 -0500

    added delete functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,33 @@
 const fs = require("fs");
 const notesData = require("../db/db.json");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
     let postNum = notesData.length;
 
-    app.get("/api/notes", function(req, res) {
+    app.get("/api/notes", function (req, res) {
         res.json(notesData);
     });
 
-    app.post("/api/notes", function(req, res) {
+    app.post("/api/notes", function (req, res) {
         let saveNote = req.body;
-        let id = postNum;
+        let id = "" + postNum;
         saveNote.id = id;
         postNum = postNum + 1;
         notesData.push(saveNote);
         res.json(saveNote);
     });
 
-    app.delete("/api/notes/:id", function(req, res) {
+    app.delete("/api/notes/:id", function (req, res) {
+        let chosen = req.params.id;
 
+        for (let i = 0; i < notesData.length; i++) {
+            if (chosen === notesData[i].id) {
+                notesData.splice(i, 1);
+            };
+        };
+
+        res.json(notesData);
 
     });
 }
