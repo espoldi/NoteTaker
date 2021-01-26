46dcd6d294185eb9b15afbe90e4c57d099d3b3be
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-01-25 21:51:59 -0500

    added get function for api

---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const notesData = require("../db/db.json");
+
+module.exports = function(app) {
+
+    app.get("/api/notes", function(req, res) {
+        res.json(notesData);
+    });
+
+}
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // Call dependencies
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = 8080;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // Listening to port
