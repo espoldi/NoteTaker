0dafe76453af2fef98ca6e4b72cb7727b98f3eff
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-01-25 22:08:18 -0500

    added notes ids

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,13 +3,23 @@ const notesData = require("../db/db.json");
 
 module.exports = function(app) {
 
+    let postNum = notesData.length;
+
     app.get("/api/notes", function(req, res) {
         res.json(notesData);
     });
 
     app.post("/api/notes", function(req, res) {
         let saveNote = req.body;
+        let id = postNum;
+        saveNote.id = id;
+        postNum = postNum + 1;
         notesData.push(saveNote);
         res.json(saveNote);
+    });
+
+    app.delete("/api/notes/:id", function(req, res) {
+
+
     });
 }
