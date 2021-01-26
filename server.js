bb1d9489da1caa7bec937fabe94082368efca958
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-01-25 20:52:28 -0500

    added server paths

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+require("./path/apiRoutes")(app);
+require("./path/htmlRoutes")(app);
 
 // Listening to port
 app.listen(PORT, function() {
