a315c7df96f14d53c0db3e79a9488997a1b64c3c
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-01-25 20:45:27 -0500

    added basic server setup

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+// Call dependencies
 const express = require("express");
+const app = express();
+const PORT = 8080;
+
+
+// Listening to port
+app.listen(PORT, function() {
+    console.log("App listening to PORT " + PORT);
+});
