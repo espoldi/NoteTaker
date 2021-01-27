a1454d1765fd1aef8192a9139c4f431f9631d792
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-01-26 20:44:36 -0500

    edited port to make it dynamic instead of static

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 8080;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
